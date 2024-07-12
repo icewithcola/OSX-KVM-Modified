42712061dfd8c37260824c0b81158d17369b4057
icewithcola <me@kagurach.uk>
2024-07-13 01:10:37 +0800

    rm

--- scripts/run_offline.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /Volumes/macOS
-mkdir -p private/tmp
-cp -R "/Install macOS Ventura.app" private/tmp
-cd "private/tmp/Install macOS Ventura.app"
-mkdir Contents/SharedSupport
-cp -R /Volumes/InstallAssistant/InstallAssistant.pkg Contents/SharedSupport/SharedSupport.dmg
-./Contents/MacOS/InstallAssistant_springboard
